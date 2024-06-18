d01d13945365c7a907080ad675956a270ba023de
TomoyaWaniko <alligatorfree12@gmail.com>
2024-06-18 14:30:48 +0900

    update

---
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/org/example/onespringbootdemo/OneSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.onespringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneSpringBootDemoApplication.class, args);
+	}
+
+}
