1f28507d818f563ecd7296dfdf0cd74872cb5768
TomoyaWaniko <alligatorfree12@gmail.com>
2024-06-20 13:28:54 +0900

    update

---
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/org/example/springcoredemo/config/SportConfig.java
@@ -0,0 +1,16 @@
+package org.example.springcoredemo.config;
+
+
+import org.example.springcoredemo.common.Coach;
+import org.example.springcoredemo.common.SwimCoach;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SportConfig {
+
+    @Bean
+    public Coach swimCoach() {
+        return new SwimCoach();
+    }
+}
