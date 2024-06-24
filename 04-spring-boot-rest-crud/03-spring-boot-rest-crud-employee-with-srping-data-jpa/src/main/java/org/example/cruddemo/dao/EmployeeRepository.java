318aebf168cb08c2039931c240b547c598c57b67
TomoyaWaniko <alligatorfree12@gmail.com>
2024-06-24 12:06:29 +0900

    update

---
+++ 04-spring-boot-rest-crud/03-spring-boot-rest-crud-employee-with-srping-data-jpa/src/main/java/org/example/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package org.example.cruddemo.dao;
+
+import org.example.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+
+}
