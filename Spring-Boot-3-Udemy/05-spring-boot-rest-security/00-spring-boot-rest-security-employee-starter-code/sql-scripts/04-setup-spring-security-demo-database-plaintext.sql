625f043a3d1a99665b058dfacb32066b0280713f
TomoyaWaniko <alligatorfree12@gmail.com>
2024-06-27 06:08:58 +0900

    update

--- Spring-Boot-3-Udemy/05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/sql-scripts/04-setup-spring-security-demo-database-plaintext.sql
+++ Spring-Boot-3-Udemy/05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/sql-scripts/04-setup-spring-security-demo-database-plaintext.sql
@@ -45,7 +45,7 @@ VALUES
 ('john','ROLE_EMPLOYEE'),
 ('mary','ROLE_EMPLOYEE'),
 ('mary','ROLE_MANAGER'),
-('susan','ROLE_EMPLOYEE'),
+('susan','ROLE_EMPLOYEE'),student
 ('susan','ROLE_MANAGER'),
 ('susan','ROLE_ADMIN');
 
--- Spring-Boot-3-Udemy/05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ Spring-Boot-3-Udemy/05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -8,17 +8,32 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    // add support for JDBC ... no more hardcoded user
+    // tell Spring Security to use JDBC authentication with our data source
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        UserDetails john = User.builder().username("john").password("{noop}test123").roles("EMPLOYEE").build();
-        UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE", "MANAGER").build();
-        UserDetails susan = User.builder().username("susan").password("{noop}test123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // define query to retrieve a user by username (how to find user)
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?"
+        );
+
+        // define query to retrieve the authorities/roles by username (how to find roles)
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?"
+        );
+
+        return jdbcUserDetailsManager;
     }
 
     @Bean
@@ -30,13 +45,24 @@ public class DemoSecurityConfig {
                 .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
                 .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
         );
-        // use HTTP Basic authentication
+        // use HTTP basic authentication
         httpSecurity.httpBasic(Customizer.withDefaults());
 
-        // disable Cross Site Request Forgery (CSRF)
-        // in general, not required for stateless REST APIs that user POST, PUT, DELETE and/or PATCH
+        // disable Cross Site Request Forgery　(CSRF)
+        // in general, not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
         httpSecurity.csrf(csrf -> csrf.disable());
 
         return httpSecurity.build();
     }
+
+    /*
+  @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails john = User.builder().username("john").password("{noop}test123").roles("EMPLOYEE").build();
+        UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE", "MANAGER").build();
+        UserDetails susan = User.builder().username("susan").password("{noop}test123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+ */
 }
